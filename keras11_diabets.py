18f8d64319513f29d96417deb891c67d31a7dbeb
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2021-11-25 19:52:33 +0900

    Update keras11_diabets.py

--- keras11_diabets.py
+++ keras11_diabets.py
@@ -18,19 +18,21 @@ print(x.shape, y.shape) #(442,10) (442,)
 print(datasets.feature_names)
 print(datasets.DESCR)
 '''
-x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.7, shuffle=True, random_state=66)
+x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.8, shuffle=True, random_state=49)
 
 model = Sequential() 
-model.add(Dense(1, input_dim=10))
-model.add(Dense(5))
+model.add(Dense(70, input_dim=10))
+model.add(Dense(60))
+model.add(Dense(50))
+model.add(Dense(40))
+model.add(Dense(30))
+model.add(Dense(20))
 model.add(Dense(10))
-model.add(Dense(5))
-model.add(Dense(3))
 model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=2000, batch_size=10,verbose=0)
+model.fit(x_train, y_train, epochs=30, batch_size=10,verbose=0)
 
 loss=model.evaluate(x_test, y_test) 
 print('loss :', loss)
@@ -42,8 +44,8 @@ r2=r2_score(y_test, y_predict)
 print('r2스코어:', r2)
 
 #result
-#loss : 3003.17138671875
-#r2스코어: 0.5179786787113398
+#loss : 2114.4248046875
+#r2스코어: 0.6031717410349849 
 
 #0.62  이상
 #R2 
