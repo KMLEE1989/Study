b8d54dd8434740c75aa19cc7ddafc28d125acde5
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2021-12-03 18:58:21 +0900

    Add files via upload

---
+++ keras/keras25_2_load_model.py
@@ -0,0 +1,48 @@
+from tensorflow.keras.models import Sequential, Model, load_model #함수형 모델
+from tensorflow.keras.layers import Dense
+import numpy as np 
+#import matplotlib.pyplot as plt 
+from sklearn.datasets import load_boston 
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import time
+
+datasets = load_boston()
+x=datasets.data
+y=datasets.target
+
+'''
+print(x)
+print(y)
+print(x.shape)
+print(y.shape)
+
+print(datasets.feature_names)
+print(datasets.DESCR)
+'''
+x_train, x_test, y_train, y_test= train_test_split(x,y,train_size=0.7, shuffle=True, random_state=66)
+
+# model = Sequential() 
+# model.add(Dense(40, input_dim=13))
+# model.add(Dense(30))
+# model.add(Dense(20))
+# model.add(Dense(10))
+# model.add(Dense(1))
+# model.summary()
+#model.save("./_save/keras25_1_save_model.h5")
+
+model = load_model('./_save/keras25_1_save_model.h5')
+model.summary()
+
+model.compile(loss='mse', optimizer='adam')
+hist = model.fit(x_train, y_train, epochs=1000, batch_size=13, validation_split=0.5)  #로스와 발로스 반환
+
+#4.
+loss=model.evaluate(x_test, y_test) 
+print('loss :', loss)
+
+y_predict= model.predict(x_test)
+
+from sklearn.metrics import r2_score
+r2=r2_score(y_test, y_predict)
+print('r2스코어:', r2)
