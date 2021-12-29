50eedd2ba20d6d5ba05f6fcb5fdca408aed8b184
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2021-12-29 09:22:06 +0900

    Update keras48_2_horse-or-human_npy.py

--- keras/keras48_2_horse-or-human_npy.py
+++ keras/keras48_2_horse-or-human_npy.py
@@ -18,10 +18,9 @@ model.add(Conv2D(32, (2,2), input_shape=(150, 150, 3), activation='relu'))
 model.add(Conv2D(32, (2,2), activation='relu')) 
 model.add(Flatten()) 
 model.add(Dense(16, activation='relu')) 
-model.add(Dense(1, activation='sigmoid')) 
+model.add(Dense(2, activation='sigmoid')) 
 
 
- 
 # 3. 컴파일, 훈련 
 model.compile(loss='binary_crossentropy', optimizer= 'adam', metrics=['accuracy']) 
 
@@ -48,42 +47,73 @@ val_acc = hist.history['val_accuracy']
 loss = hist.history['loss'] 
 val_loss = hist.history['val_loss'] 
  
-# 그래프 그려 보기 
-print('loss:', loss[-1]) 
-print('val_loss:', val_loss[-1]) 
-print('acc:', accuracy[-1]) 
-print('val_acc:',val_acc [-1]) 
+# # 그래프 그려 보기 
+# print('loss:', loss[-1]) 
+# print('val_loss:', val_loss[-1]) 
+# print('acc:', accuracy[-1]) 
+# print('val_acc:',val_acc [-1]) 
  
-epochs = range(1, len(loss)+1) 
-import matplotlib.pyplot as plt 
-plt.plot(epochs, loss, 'r--', label = 'loss') 
-plt.plot(epochs, val_loss, 'r:', label = 'val_loss') 
-plt.plot(epochs, accuracy, 'b--', label = 'acc') 
-plt.plot(epochs, val_acc, 'b:', label = 'val_acc') 
+# epochs = range(1, len(loss)+1) 
+# import matplotlib.pyplot as plt 
+# plt.plot(epochs, loss, 'r--', label = 'loss') 
+# plt.plot(epochs, val_loss, 'r:', label = 'val_loss') 
+# plt.plot(epochs, accuracy, 'b--', label = 'acc') 
+# plt.plot(epochs, val_acc, 'b:', label = 'val_acc') 
  
-plt.grid() 
-plt.legend() 
-plt.show 
+# plt.grid() 
+# plt.legend() 
+# plt.show 
  
- 
-# summarize history for accuracy 
-plt.plot(accuracy) 
-plt.plot(val_acc) 
-plt.title('model accuracy') 
-plt.ylabel('accuracy') 
-plt.xlabel('epoch') 
-plt.legend(['train', 'test'], loc='upper left') 
-plt.show() 
-# summarize history for loss 
-plt.plot(loss) 
-plt.plot(val_loss) 
-plt.title('model loss') 
-plt.ylabel('loss') 
-plt.xlabel('epoch') 
-plt.legend(['train', 'test'], loc='upper left') 
-plt.show()
+# # summarize history for accuracy 
+# plt.plot(accuracy) 
+# plt.plot(val_acc) 
+# plt.title('model accuracy') 
+# plt.ylabel('accuracy') 
+# plt.xlabel('epoch') 
+# plt.legend(['train', 'test'], loc='upper left') 
+# plt.show() 
+# # summarize history for loss 
+# plt.plot(loss) 
+# plt.plot(val_loss) 
+# plt.title('model loss') 
+# plt.ylabel('loss') 
+# plt.xlabel('epoch') 
+# plt.legend(['train', 'test'], loc='upper left') 
+# plt.show()
+
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+pic_path = 'D:\\_data\\image\\NA\\JAE HO LEE.jpg'
+model_path = 'D:\\Study\\_ModelCheckPoint\\k26_1228_1751_0010-1.5944.hdf5'
+
+def load_my_image(img_path,show=False):
+    img = image.load_img(img_path, target_size=(150,150))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis = 0)
+    img_tensor /=255.
+    
+    if show:
+        plt.imshow(img_tensor[0])    
+        # plt.append('off')
+        plt.show()
+    
+    return img_tensor
+
+if __name__ == '__main__':
+    # model = load_model(model_path)
+    new_img = load_my_image(pic_path)
+    pred = model.predict(new_img)
+    horse = pred[0][0]*100
+    human = pred[0][1]*100
+    if human > horse:
+        print(f"당신은 {round(human,2)} % 확률로 말 입니다")
+    else:
+        print(f"당신은 {round(horse,2)} % 확률로 인간 입니다")
 
 # loss: 0.6927468180656433
 # val_loss: 0.6928723454475403
 # acc: 0.7142857313156128
 # val_acc: 0.6666666865348816
+# 당신은 50.19 % 확률로 말 입니다
