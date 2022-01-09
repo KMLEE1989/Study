526b1b566454cefe048f2ce62ff9af89de3c8156
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-01-09 15:09:57 +0900

    Add files via upload

---
+++ keras/personal project 기온 시각화.py
@@ -0,0 +1,60 @@
+import matplotlib
+import pandas as pd
+import numpy as np
+import numpy as np
+from numpy.core.fromnumeric import shape
+import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint 
+import time
+from tensorflow.keras.layers import Dropout, Dense, SimpleRNN,LSTM
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import matplotlib as mpl
+import csv
+import matplotlib.font_manager as fm
+import matplotlib_inline
+
+path = "../_data/개인프로젝트/CSV/"
+
+df1=pd.read_csv(path+'기온 데이터.csv',thousands=',')
+df1=df1.drop(['지점'], axis=1)
+print(df1.info())
+
+# Data columns (total 4 columns):
+#  #   Column   Non-Null Count  Dtype
+# ---  ------   --------------  -----
+#  0   DATE     712 non-null    object
+#  1   평균기온(℃)  712 non-null    float64
+#  2   최저기온(℃)  712 non-null    float64
+#  3   최고기온(℃)  712 non-null    float64
+
+df1['DATE']=df1['DATE'].astype('datetime64[ns]')
+
+font_path = "C:/Windows/Fonts/HMFMMUEX.TTC"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
+fontprop=fm.FontProperties(fname=font_path, size=18)
+
+# print(df1.sort_values(by='평균기온(℃)').head())
+# print(df1.sort_values(by='최저기온(℃)').head())
+# print(df1.sort_values(by='최고기온(℃)').head())
+
+plt.figure(figsize=(1000,1000))
+plt.plot(df1['DATE'], df1['평균기온(℃)'], df1['DATE'], df1['최저기온(℃)'], df1['DATE'],df1['최고기온(℃)'],'rs-', marker='o')
+plt.title('날짜별 기온', font=fontprop)
+plt.ylabel('기온', font=fontprop)
+plt.xlabel('DATE', font=fontprop)
+plt.legend(['average','MAX','MIN'], loc='upper right', ncol=1, fontsize=10)
+plt.grid()
+plt.show()
+
+# y1=df1
+# y1=y1.drop(['DATE', '지점'], axis=1)
+# print(y1.columns)
+# print(y1.info())
+
+#  #   Column   Non-Null Count  Dtype
+# ---  ------   --------------  -----
+#  0   평균기온(℃)  712 non-null    float64
+#  1   최저기온(℃)  712 non-null    float64
+#  2   최고기온(℃)  712 non-null    float64
