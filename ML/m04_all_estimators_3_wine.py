b6198e5e8cabf6424743a161c986d2169cbd86df
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-01-18 09:11:09 +0900

    Add files via upload

---
+++ ML/m04_all_estimators_3_wine.py
@@ -0,0 +1,74 @@
+from sklearn import datasets
+from sklearn.utils import all_estimators  #회기모델regressor은 r2스코어 accuracy= classifier
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_wine
+import warnings 
+warnings.filterwarnings('ignore')
+
+datasets=load_wine()
+x=datasets.data
+y=datasets.target
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test= train_test_split(x,y,train_size=0.8, shuffle=True, random_state=66) 
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train=scaler.transform(x_train)
+x_test=scaler.transform(x_test)
+
+allAlgorithm = all_estimators(type_filter='classifier')
+#allAlgorithms = all_estimators(type_filter='regressor')
+
+
+print("allAlgorithms : ", allAlgorithm)
+print("모델의 갯수: ", len(allAlgorithm)) #3대장이 없어
+
+for (name, algorithm) in allAlgorithm:
+    try:
+        model = algorithm()
+        model.fit(x_train, y_train)
+        
+        y_predict = model.predict(x_test)
+        acc = accuracy_score(y_test, y_predict)
+        print(name, '의 정답률 : ', acc)
+
+    except:
+        continue
+    
+# 모델의 갯수:  41
+# AdaBoostClassifier 의 정답률 :  0.8888888888888888
+# BaggingClassifier 의 정답률 :  1.0
+# BernoulliNB 의 정답률 :  0.4166666666666667
+# CalibratedClassifierCV 의 정답률 :  0.9722222222222222
+# CategoricalNB 의 정답률 :  0.5
+# ComplementNB 의 정답률 :  0.8611111111111112
+# DecisionTreeClassifier 의 정답률 :  0.9444444444444444
+# DummyClassifier 의 정답률 :  0.4166666666666667
+# ExtraTreeClassifier 의 정답률 :  1.0
+# ExtraTreesClassifier 의 정답률 :  1.0
+# GaussianNB 의 정답률 :  1.0
+# GaussianProcessClassifier 의 정답률 :  1.0
+# GradientBoostingClassifier 의 정답률 :  0.9722222222222222
+# HistGradientBoostingClassifier 의 정답률 :  0.9722222222222222
+# KNeighborsClassifier 의 정답률 :  1.0
+# LabelPropagation 의 정답률 :  1.0
+# LabelSpreading 의 정답률 :  1.0
+# LinearDiscriminantAnalysis 의 정답률 :  1.0
+# LinearSVC 의 정답률 :  0.9722222222222222
+# LogisticRegression 의 정답률 :  1.0
+# LogisticRegressionCV 의 정답률 :  0.9722222222222222
+# MLPClassifier 의 정답률 :  1.0
+# MultinomialNB 의 정답률 :  0.9444444444444444
+# NearestCentroid 의 정답률 :  1.0
+# NuSVC 의 정답률 :  1.0
+# PassiveAggressiveClassifier 의 정답률 :  0.9722222222222222
+# Perceptron 의 정답률 :  0.9722222222222222
+# QuadraticDiscriminantAnalysis 의 정답률 :  0.9722222222222222
+# RadiusNeighborsClassifier 의 정답률 :  0.9722222222222222
+# RandomForestClassifier 의 정답률 :  1.0
+# RidgeClassifier 의 정답률 :  1.0
+# RidgeClassifierCV 의 정답률 :  0.9722222222222222
+# SGDClassifier 의 정답률 :  0.9722222222222222
+# SVC 의 정답률 :  1.0
