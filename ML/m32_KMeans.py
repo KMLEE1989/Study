aa2745e14ed9ab51611d1a7d0087ffeb586e59af
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-02-04 11:01:45 +0900

    Add files via upload

---
+++ ML/m32_KMeans.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_iris  
+from sklearn.cluster import KMeans # y가 없는 거 비지도 학습
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+datasets=load_iris()
+
+# x = datasets.data
+# y = datasets.target
+
+x=pd.DataFrame(datasets.data, columns=[datasets.feature_names])
+
+#print(type(x))
+print(x)
+
+kmeans = KMeans(n_clusters=3)  #분류에서만 가능
+kmeans.fit(x)
+
+print(kmeans.labels_)
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+#  1 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 1 2 2 2 2 1 2 2 2 2
+#  2 2 1 1 2 2 2 2 1 2 1 2 1 2 2 1 1 2 2 2 2 2 1 2 2 2 2 1 2 2 2 1 2 2 2 1 2
+#  2 1]
+
+print(datasets.target)
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+#  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2
+#  2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+#  2 2]
+
