c8d3d1381d89715b4d4b0223b6898d1915922bd8
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-02-05 09:37:38 +0900

    Add files via upload

---
+++ ML/m32_KMeans1_iris.py
@@ -0,0 +1,46 @@
+from sklearn import cluster
+from sklearn.datasets import load_iris  
+from sklearn.cluster import KMeans # y가 없는 거 비지도 학습
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+datasets=load_iris()
+
+# x = datasets.data
+# y = datasets.target
+
+irisDF=pd.DataFrame(datasets.data, columns=[datasets.feature_names])
+
+#print(type(x))
+print(irisDF)
+
+kmeans = KMeans(n_clusters=3, random_state=66)  #분류에서만 가능
+kmeans.fit(irisDF)
+
+print(kmeans.labels_)
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+#  1 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 1 2 2 2 2 1 2 2 2 2
+#  2 2 1 1 2 2 2 2 1 2 1 2 1 2 2 1 1 2 2 2 2 2 1 2 2 2 2 1 2 2 2 1 2 2 2 1 2
+#  2 1]
+
+print(datasets.target)
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+#  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2
+#  2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+#  2 2]
+
+irisDF['cluster'] = kmeans.labels_
+irisDF['target'] = datasets.target
+
+# accuracy_score 구해라
+cluster=irisDF['cluster']
+target=irisDF['target']
+
+print(accuracy_score(cluster, target))
+
+
+# accuracy_score
+# 0.8933333333333333
