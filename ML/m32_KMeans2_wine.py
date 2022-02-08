fefd58f8581662f614dbbc010a69c9f1680d5196
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-02-08 09:26:08 +0900

    Add files via upload

---
+++ ML/m32_KMeans2_wine.py
@@ -0,0 +1,43 @@
+from sklearn.datasets import load_iris  
+from sklearn.datasets import load_wine
+from sklearn.cluster import KMeans # y가 없는 거 비지도 학습
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+datasets=load_wine()
+
+# x = datasets.data
+# y = datasets.target
+
+wineDF=pd.DataFrame(datasets.data, columns=[datasets.feature_names])
+
+#print(type(x))
+print(wineDF)
+
+kmeans = KMeans(n_clusters=3, random_state=66)  #분류에서만 가능
+kmeans.fit(wineDF)
+
+print(kmeans.labels_)
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+#  1 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 1 2 2 2 2 1 2 2 2 2
+#  2 2 1 1 2 2 2 2 1 2 1 2 1 2 2 1 1 2 2 2 2 2 1 2 2 2 2 1 2 2 2 1 2 2 2 1 2
+#  2 1]
+
+print(datasets.target)
+# [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#  0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+#  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 2
+#  2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+#  2 2]
+
+wineDF['cluster']=kmeans.labels_
+wineDF['target']=datasets.target
+
+cluster = wineDF['cluster']
+target=wineDF['target']
+
+print(accuracy_score(cluster, target))
+
+# accuracy score:0.1853932584269663
