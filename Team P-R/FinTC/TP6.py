eabe1e33c155395a64341601aa14324b72d86346
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-02-18 21:53:40 +0900

    Add files via upload

---
+++ Team P-R/FinTC/TP6.py
@@ -0,0 +1,30 @@
+#for
+
+# for i in range(0, 10):
+#     print("%s 일째 복역 중 입니다." % i)
+#
+#     if i == 5:
+#         print("사면되었습니다.")
+#         break;
+#
+# print("%s 번 다 복역해서 자유가 되었습니다. 나왔습니다!" % len(range(0, 6)))
+
+
+# for i in range(0, 10):
+#     count = 0
+#     count += 1
+#
+# print(count)
+
+for i, value in enumerate(['키움', '삼성', '카카오']):
+    print("%s 순번과 %s 값" % (i, value))
+
+# 0 순번과 키움 값
+# 1 순번과 삼성 값
+# 2 순번과 카카오 값
+
+
+
+
+
+
