5aa4e4aab64ea67d6dcee2c92556a35ff2ef2949
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-02-18 22:15:47 +0900

    Add files via upload

---
+++ Team P-R/FinTC/TP7.py
@@ -0,0 +1,31 @@
+# while
+
+# cnt = 0
+# while True:
+#     print("마법을 쓰기 위해 %s번째 시도를 하였다." % cnt)
+#
+#     if cnt == 1000:
+#         print("%s 마법을 써서 빠져나간다!" % cnt)
+#         break
+#
+#     cnt += 1
+
+
+
+# cnt = 0
+# repeat_boolean = True
+# while repeat_boolean:
+#     print("%s 아직은 while문 도는 중" % cnt)
+#
+#     cnt += 1
+#
+#     if cnt == 100:
+#         repeat_boolean = False
+
+stock_price =1000
+while stock_price != 10000000:
+
+    stock_price += 1000
+
+    print("방금 %s 원을 더해서 %s원이 되었다." % (1000, stock_price))
+
