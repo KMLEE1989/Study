d4998736512968371d0e5876b31b19ae23d1bc5e
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-02-18 15:00:08 +0900

    Add files via upload

---
+++ Team P-R/FinTC/TP4.py
@@ -0,0 +1,16 @@
+# # < , <=
+# stock_price = 3000
+# if stock_price < 2000:
+#     print("2000보다 작네요!")
+#
+# elif stock_price <= 3000:
+#     print("3000보다 같거나 작네요!")
+#
+# # > , >=
+# stock_price = 2.5
+# if stock_rate > 2.5:
+#     print("2.5보다 크네요!")
+#
+# elif stock_rate >= 2.5 :
+#     print("2.5 이상이냐?")
+
