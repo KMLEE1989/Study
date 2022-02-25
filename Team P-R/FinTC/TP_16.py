fc9401ed29748d22346ce52af0f7ca79944eb418
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-02-25 14:08:54 +0900

    Add files via upload

---
+++ Team P-R/FinTC/TP_16.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#class 기초
+class B_school():
+    def __init__(self):
+        print("B 클래스 입니다.")
+
+        self.student_name = '원빈'
+
+        print(dir(self))
+
+
+B_school()
