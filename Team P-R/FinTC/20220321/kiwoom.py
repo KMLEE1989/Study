0cf5e7273dab8f8746ae43cebc102b6bd8951d72
KMLEE1989 <94456661+KMLEE1989@users.noreply.github.com>
2022-03-21 12:05:43 +0900

    Add files via upload

---
+++ Team P-R/FinTC/20220321/kiwoom.py
@@ -0,0 +1,31 @@
+from PyQt5.QAxContainer import *
+
+class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+
+        print("Kiwoom 클래스 입니다")
+
+        self.get_ocx_instance()
+        self.event_slots()
+
+        self.signal_login_commConnect()
+
+    def get_ocx_instance(self):
+        self.setControl('KHOPENAPI.KHOpenAPICtrl.1')
+
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+
+    def login_slot(self, errCode):
+        print(errCode)
+
+    def signal_login_commConnect(self):
+        self.dynamicCall("CommConnect()")
+
+
+
+
+
+
+
