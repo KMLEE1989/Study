0bc8d803bbf33cf838c85784b6e8e4c5edd97d74
JAE HO LEE <94456661+KMLEE1989@users.noreply.github.com>
2022-06-24 09:49:37 +0900

    Add files via upload

---
+++ GUI/GUI_Practice_2.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+import tkinter
+
+win = tkinter.Tk()
+win.geometry('300x150')
+label = tkinter.Label(win, text='라벨 입니다.')
+label.pack()
+win.mainloop()
