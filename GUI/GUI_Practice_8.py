a9235b0f6f41dc70b79cf3bb2e6ce7a26539baa8
JAE HO LEE <94456661+KMLEE1989@users.noreply.github.com>
2022-07-05 13:14:49 +0900

    Add files via upload

---
+++ GUI/GUI_Practice_8.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+
+import tkinter
+from tkinter import messagebox
+
+def btnClick():
+    txt = text.get()
+    messagebox.showwarning('test', txt)
+    
+win = tkinter.Tk()
+win.geometry('300x150')
+label = tkinter.Label(win, text = '라벨 입니다.')
+label.grid(row=0, column=0, pady=10)
+text = tkinter.Entry(win)
+text.grid(row=1, column=0, padx=10)
+button = tkinter.Button(win, text='버튼입니다.', command=btnClick)
+button.grid(row=1, column=1)
+win.mainloop()
+    
+    
