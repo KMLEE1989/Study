a162bf85c3d3a82648a820a6798f997b057bb99c
JAE HO LEE <94456661+KMLEE1989@users.noreply.github.com>
2022-08-04 16:30:59 +0900

    Add files via upload

---
+++ GUI/python_GUI_35.py
@@ -0,0 +1,37 @@
+## Ex 5-12. QPixmap.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        pixmap = QPixmap('C:\\picture\\2.png')
+
+
+        lbl_img = QLabel()
+        lbl_img.setPixmap(pixmap)
+        lbl_size = QLabel('Width: '+str(pixmap.width())+', Height: '+str(pixmap.height()))
+        lbl_size.setAlignment(Qt.AlignCenter)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl_img)
+        vbox.addWidget(lbl_size)
+        self.setLayout(vbox)
+
+        self.setWindowTitle('QPixmap')
+        self.move(920, 761)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
