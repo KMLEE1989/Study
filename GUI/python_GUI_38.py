c67724c08cf63d1fa4b7ee58a4059c880be46c1b
JAE HO LEE <94456661+KMLEE1989@users.noreply.github.com>
2022-08-08 20:49:36 +0900

    Add files via upload

---
+++ GUI/python_GUI_38.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QDateEdit, QVBoxLayout
+from PyQt5.QtCore import QDate
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        lbl = QLabel('QDateEdit')
+
+        dateedit = QDateEdit(self)
+        dateedit.setDate(QDate.currentDate())
+        dateedit.setMinimumDate(QDate(1900, 1, 1))
+        dateedit.setMaximumDate(QDate(2100, 12, 31))
+        # dateedit.setDateRange(QDate(1900, 1, 1), QDate(2100, 12, 31))
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl)
+        vbox.addWidget(dateedit)
+        vbox.addStretch()
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle('QDateEdit')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
