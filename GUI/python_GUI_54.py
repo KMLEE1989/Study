6597e65e15fc6e71359d3b894b53016c0c2281e5
JAE HO LEE <94456661+KMLEE1989@users.noreply.github.com>
2022-09-01 10:41:05 +0900

    Add files via upload

---
+++ GUI/python_GUI_54.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtCore import Qt
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 400, 300)
+        self.setWindowTitle('Points')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_point(qp)
+        qp.end()
+
+    def draw_point(self, qp):
+        qp.setPen(QPen(Qt.blue,  8))
+        qp.drawPoint(self.width()/2, self.height()/2)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
