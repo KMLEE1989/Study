aea2efd8feef6c9d6ef5993869f0d1483d0c0e66
JAE HO LEE <94456661+KMLEE1989@users.noreply.github.com>
2022-09-02 05:31:37 +0900

    Add files via upload

---
+++ GUI/python_GUI_55.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtCore import Qt
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 400, 300)
+        self.setWindowTitle('drawLine')
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_line(qp)
+        qp.end()
+
+    def draw_line(self, qp):
+        qp.setPen(QPen(Qt.blue, 8))
+        qp.drawLine(30, 230, 200, 50)
+        qp.setPen(QPen(Qt.green, 12))
+        qp.drawLine(140, 60, 320, 280)
+        qp.setPen(QPen(Qt.red, 16))
+        qp.drawLine(330, 250, 40, 190)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
